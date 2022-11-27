7af78f4fb7d8695b41e56f6a18fe13ce62487804
Lans turu ygy <111067376+Lans-ofc@users.noreply.github.com>
2022-11-27 21:23:10 +0700

    Update main-menu2.js

--- plugins/main-menu2.js
+++ plugins/main-menu2.js
@@ -8,17 +8,17 @@ let suka = `Saya Putra`
 global.fkontak = { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: `status@broadcast` } : {}) }, message: { 'contactMessage': { 'displayName': suka, 'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:XL;${suka},;;;\nFN:${suka},\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabell:Ponsel\nEND:VCARD`, 'jpegThumbnail': fs.readFileSync('./thumbnail.jpg'), thumbnail: fs.readFileSync('./thumbnail.jpg'),sendEphemeral: true}}}
 
 let str = `*Hai Ngab*`
-let isi =`I'm Kaylabotz Yang Di Buat Gak Niat Oleh Putra Karena Males Recode Jadi Pake Aja Apa Yang Ada Yah.\nKalau Ada Yg Error Bisa Langsung Report Ke Owner.\n${tag} (Owner)`
+let isi =`ɪ'ᴍ xʏꜱʙᴏᴛ ʏᴀɴɢ ᴅɪ ʙᴜᴀᴛ ɢᴀᴋ ɴɪᴀᴛ ᴏʟᴇʜ ʟᴀɴꜱ ᴋᴀʀᴇɴᴀ ᴍᴀʟᴇꜱ ʀᴇᴄᴏᴅᴇ ᴊᴀᴅɪ ᴘᴀᴋᴇ ᴀᴊᴀ ᴀᴘᴀ ʏᴀɴɢ ᴀᴅᴀ ʏᴀʜ.\nKalau Ada Yg Error Bisa Langsung Report Ke Owner.\n #Owner Ganteng🗿`
 let wibu = `https://api-reysekha.herokuapp.com/api/random/cosplay?apikey=apirey` 
 let thumb = await(await fetch(wibu)).buffer()
 conn.send3ButtonImg(m.chat, `https://telegra.ph/file/116c1a217394d408415c9.jpg`, str, isi, 'Menu', '.command', 'Owner', '.owner', 'Credit', '.credit', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: 'https://instagram.com/l4c1f2r',
+    mediaUrl: 'https://instagram.com/lans.xys',
     mediaType: 2, 
     description: urlnya,
     title: "Follow Ig Gw",
     body: wm,
     thumbnail: thumb,
-    sourceUrl: 'https://instagram.com/l4c1f2r'
+    sourceUrl: 'https://instagram.com/lans.xys'
      }}
   })
           }
@@ -26,6 +26,6 @@ handler.help = ['menu']
 handler.tags = ['main']
 handler.command =  /^(menu)$/i
 
-handler.register = true
+handler.register = false
 
 module.exports = handler
